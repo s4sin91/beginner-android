2583542e9016e3e07fc3abc183e2dbe04145a91f
singh <98948331+s4sin91@users.noreply.github.com>
2022-03-04 19:47:13 +0530

    Update WordAdpater.java

--- Multiple screens/Basic-concepts/Custom-ArrayAdapter/WordAdpater.java
+++ Multiple screens/Basic-concepts/Custom-ArrayAdapter/WordAdpater.java
@@ -1,31 +1,30 @@
-package com.mycompany.state;
-
+package com.b4brar.puntrans;
 import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import com.b4brar.puntrans.Akkhar;
 import java.util.ArrayList;
 
-public class WordAdapter extends ArrayAdapter<Word>{
-	int totalT=0;
-	public WordAdapter(Activity context, ArrayList<Word> words) {
-		super(context, 0, words);
+public class AkkharAdapter extends ArrayAdapter<Akkhar>{
+	AkkharAdapter(Activity context, ArrayList<Akkhar> akkhar){
+		super(context,0,akkhar);
 	}
+
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View listItemView = convertView;
-		if (listItemView ==null) {
-			listItemView = LayoutInflater.from(getContext()).inflate(
-				R.layout.list_item,parent, false);
+		if(listItemView==null){
+			listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item,parent,false);
 		}
-		System.out.println("getView " + position+" "+convertView);
-		Word currentWord = getItem(position);
-		TextView nameTextView = (TextView) listItemView.findViewById(R.id.punjabiText);
-		nameTextView.setText(currentWord.getPunjabiText());
-		TextView numberTextView = (TextView) listItemView.findViewById(R.id.englishText);
-		numberTextView.setText(currentWord.getEnglishText());
+		Akkhar currentAkkhar = getItem(position);
+		TextView punTextView = (TextView)listItemView.findViewById(R.id.punjabiText);
+		punTextView.setText(currentAkkhar.getEnglish());
+		TextView engTextView = (TextView)listItemView.findViewById(R.id.englishText);
+	    engTextView.setText(currentAkkhar.getPunjabi());
 		return listItemView;
-		}
+	}
+    
 }
